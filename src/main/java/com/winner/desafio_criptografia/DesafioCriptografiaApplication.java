2b433cfcfb220636b17c09471e5ec373773832ad
Wallace Winner <wallacewinner@hotmail.com>
2026-05-11 20:01:01 -0300

    Add initial project structure

---
+++ src/main/java/com/winner/desafio_criptografia/DesafioCriptografiaApplication.java
@@ -0,0 +1,13 @@
+package com.winner.desafio_criptografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCriptografiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCriptografiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winner/desafio_criptografia/DesafioCriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winner.desafio_criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
